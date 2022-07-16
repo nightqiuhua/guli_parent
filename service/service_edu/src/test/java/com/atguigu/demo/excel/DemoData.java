9e12f03dcffc46e7d5cc4e5940aee79c87d4d556
nightqiuhua <1320551630@qq.com>
2022-07-16 11:40:53 +0800

    original files

--- service/service_edu/src/test/java/com/atguigu/demo/excel/DemoData.java
+++ service/service_edu/src/test/java/com/atguigu/demo/excel/DemoData.java
@@ -6,10 +6,8 @@ import lombok.Data;
 @Data
 public class DemoData {
     //设置excel表头名称
-
-    @ExcelProperty(value =  "学生编号",index = 0)
+    @ExcelProperty(value = "学生编号",index = 0)
     private Integer sno;
     @ExcelProperty(value = "学生姓名",index = 1)
-    private String name;
-
+    private String sname;
 }
